e5d4225fed8d0f0b52c12da8041948db40fbc352
Ivan Notaros <nothke@gmail.com>
2020-05-12 06:11:00 +0200

    Even more cleanup

--- unityapply.py
+++ unityapply.py
@@ -1,9 +1,6 @@
 # Makes blue axis be the forward axis, and green arrow the up axis
 # so as to use the Unity standard
 #
-# To use select objects you want to apply, and find 'Apply for Unity'
-# in the context menu (press space by default)
-#
 # By Nothke
 
 bl_info = {
@@ -21,20 +18,22 @@ from mathutils import Vector
 from mathutils import Matrix
 
 class NOTHKE_OT_unity_rotation_fix(bpy.types.Operator):
-    """Fixes rotation (swaps Y and Z axes) for exporting to Unity"""      # blender will use this as a tooltip for menu items and buttons.
-    bl_idname = "object.unity_rotation_fix"        # unique identifier for buttons and menu items to reference.
-    bl_label = "Apply Unity Rotation Fix"         # display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # enable undo for the operator.
+    """Fixes rotation (swaps Y and Z axes) for exporting to Unity"""
+    bl_idname = "object.unity_rotation_fix"
+    bl_label = "Apply Unity Rotation Fix"
+    bl_options = {'REGISTER', 'UNDO'}
     
-    def execute(self, context):        # execute() is called by blender when running the operator.
+    def execute(self, context):
 
         original_active_object = context.view_layer.objects.active
 
+        layer = context.view_layer
+
         for obj in bpy.context.selected_objects:
-            context.view_layer.objects.active = obj
+            layer.objects.active = obj
             
             # Remember original rotation
-            rot = bpy.context.object.rotation_euler
+            rot = obj.rotation_euler
             original_rotation = [rot[0], rot[1], rot[2]]
             
             # Unrotate
@@ -48,7 +47,7 @@ class NOTHKE_OT_unity_rotation_fix(bpy.types.Operator):
             rot[2] = pi * 180 / 180
             
             # Then apply this rotation, now forward is blue and up is green relative to mesh
-            context.view_layer.objects.active = obj
+            layer.objects.active = obj
             bpy.ops.object.transform_apply(location=False, rotation=True, scale=False, properties=False)
             
             # Finally, return rotation so that it's oriented properly in Blender
@@ -63,11 +62,13 @@ class NOTHKE_OT_unity_rotation_fix(bpy.types.Operator):
             # Update model matrix
             bpy.context.view_layer.update()
             
+            layer.objects.active = None
+            
         # You should see no changes in viewport, but blue and green axes have switched and rotations set
 
         context.view_layer.objects.active = original_active_object
 
-        return {'FINISHED'}            # this lets blender know the operator finished successfully.
+        return {'FINISHED'}
 
 def menu_draw(self, context):
     layout = self.layout
@@ -81,8 +82,5 @@ def unregister():
     bpy.utils.unregister_class(NOTHKE_OT_unity_rotation_fix)
     bpy.types.VIEW3D_MT_object_apply.remove(menu_draw)
 
-
-# This allows you to run the script directly from blenders text editor
-# to test the addon without having to install it.
 if __name__ == "__main__":
     register()
