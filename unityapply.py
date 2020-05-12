04d6175874ea3c613eddd75a416e9bd0145f0ec1
Ivan Notaros <nothke@gmail.com>
2020-05-12 17:39:54 +0200

    Now works with multi-selection again

--- unityapply.py
+++ unityapply.py
@@ -28,43 +28,74 @@ class NOTHKE_OT_unity_rotation_fix(bpy.types.Operator):
         original_active_object = context.view_layer.objects.active
 
         layer = context.view_layer
+        
+        print(" - ")
+        
+        #selected_objects = [ o for o in bpy.context.scene.objects if o.select ]
+        
+        selected_objects = []
+        for obj in context.selected_objects:
+            selected_objects.append(obj)
+            
+        bpy.ops.object.select_all(action='DESELECT')
+        
+        # debug
+        for obj in selected_objects:
+            print(obj.name)
+            print(obj.rotation_euler)
+            
+        print(" - - ")
+        
+        #selected_objects[0].select_set(True)
 
-        for obj in bpy.context.selected_objects:
+        for obj in selected_objects:
+            # Select object and make active
+            obj.select_set(True)
             layer.objects.active = obj
             
-            # Remember original rotation
-            rot = obj.rotation_euler
-            original_rotation = [rot[0], rot[1], rot[2]]
+            print(context.view_layer.objects.active.name)
             
+            # Remember original rotation
+            original_rotation = [obj.rotation_euler[0], obj.rotation_euler[1], obj.rotation_euler[2]]
+                  
             # Unrotate
-            bpy.context.object.rotation_euler = [0, 0, 0]
+            obj.rotation_euler = [0, 0, 0]
+            
+            print("rot ", layer.objects.active.rotation_euler)
+            print("rot ", obj.rotation_euler)
             
             # First, apply rotation so it's 0,0,0
             bpy.ops.object.transform_apply(location=False, rotation=True, scale=False, properties=False)
 
             # Then rotate the object so it points 'up'
-            rot[0] = pi * 90 / 180
-            rot[2] = pi * 180 / 180
+            obj.rotation_euler[0] = pi * 90 / 180
+            obj.rotation_euler[2] = pi * 180 / 180
             
             # Then apply this rotation, now forward is blue and up is green relative to mesh
             layer.objects.active = obj
             bpy.ops.object.transform_apply(location=False, rotation=True, scale=False, properties=False)
             
             # Finally, return rotation so that it's oriented properly in Blender
-            rot[0] = pi * -90 / 180
-            rot[1] = pi * -180 / 180
+            obj.rotation_euler[0] = pi * -90 / 180
+            obj.rotation_euler[1] = pi * -180 / 180
             
             # Apply original rotation
-            rot[0] -= original_rotation[0]
-            rot[1] += original_rotation[1]
-            rot[2] += original_rotation[2]
+            obj.rotation_euler[0] -= original_rotation[0]
+            obj.rotation_euler[1] += original_rotation[1]
+            obj.rotation_euler[2] += original_rotation[2]
+            
+            #rot = None
             
             # Update model matrix
             bpy.context.view_layer.update()
-            
+
+            # Deselect this object
             layer.objects.active = None
-            
-        # You should see no changes in viewport, but blue and green axes have switched and rotations set
+            bpy.ops.object.select_all(action='DESELECT')
+             
+        # Reselect previously selected objects
+        for obj in selected_objects:
+            obj.select_set(True)
 
         context.view_layer.objects.active = original_active_object
 
